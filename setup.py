3b0686c87a509626f1d23f16f9122847888644be
sigurd <sigurdcarlsen@gmail.com>
2019-10-30 11:48:21 +0100

    Revert "merge develop into master (#24)"
    This reverts commit 28be35c3cc0c796fc41997d27405ed478212aa1c.

--- setup.py
+++ setup.py
@@ -2,16 +2,22 @@ try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
+import versioneer
 
 
-setup(name='ProcessOptimizer',
-      version='0.3.2',
-      description='Sequential model-based optimization toolbox (forked from scikit-optimize)',
-      url='https://github.com/bytesandbrains/ProcessOptimizer',
+setup(name='scikit-optimize',
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
+      description='Sequential model-based optimization toolbox.',
+      long_description=open('README.rst').read(),
+      url='https://scikit-optimize.github.io/',
       license='BSD',
-      author='Bytes and Brains',
-      packages=['ProcessOptimizer', 'ProcessOptimizer.learning', 'ProcessOptimizer.optimizer', 'ProcessOptimizer.space',
-                'ProcessOptimizer.learning.gaussian_process'],
-      install_requires=['pyaml', 'numpy', 'matplotlib', 'scipy>=0.14.0',
-                        'scikit-learn>=0.19.1', 'bokeh']
+      author='The scikit-optimize contributors',
+      packages=['skopt', 'skopt.learning', 'skopt.optimizer', 'skopt.space',
+                'skopt.learning.gaussian_process'],
+      install_requires=['pyaml', 'numpy', 'scipy>=0.14.0',
+                        'scikit-learn>=0.19.1'],
+      extras_require={
+        'plots':  ["matplotlib"]
+        }
       )
