493c9e87423045e465e020f61d65fa4f8bb36361
SQBL (Soeren Bertelsen) <SQBL@novonordisk.com>
2020-09-08 11:00:30 +0200

    Changed searchCV to reflect skopt #904

--- ProcessOptimizer/searchcv.py
+++ ProcessOptimizer/searchcv.py
@@ -10,9 +10,17 @@ import sklearn
 from sklearn.base import is_classifier, clone
 from sklearn.model_selection._search import BaseSearchCV
 from sklearn.utils import check_random_state
-from sklearn.utils.fixes import MaskedArray
 from sklearn.utils.validation import indexable, check_is_fitted
-from sklearn.metrics.scorer import check_scoring
+
+try:
+    from sklearn.utils.fixes import MaskedArray
+except ImportError:
+    from numpy.ma import MaskedArray
+
+try:
+    from sklearn.metrics import check_scoring
+except ImportError:
+    from sklearn.metrics.scorer import check_scoring
 
 from joblib import Parallel, delayed
 
