ea7c4eac0cc4e605d35e5218a9ffbc6a7b24be16
AkselObdrup <75260256+AkselObdrup@users.noreply.github.com>
2021-03-19 09:24:03 +0100

    Multiobjective plot functionality
    Added function plot_objectives to give possibility to plot objective functions for a list of results. Furthermore added title argument to plot_objective and made a small change in plot_evaluations to ensure that the function would work with a list of results as input.

--- ProcessOptimizer/plots.py
+++ ProcessOptimizer/plots.py
@@ -390,7 +390,7 @@ def dependence(space, model, i, j=None, sample_points=None,
 
 
 def plot_objective(result, levels=10, n_points=40, n_samples=250, size=2,
-                   zscale='linear', dimensions=None,usepartialdependence=True, pars='result', expected_minimum_samples = None):
+                   zscale='linear', dimensions=None,usepartialdependence=True, pars='result', expected_minimum_samples = None, title=None):
     """Pairwise dependence plot of the objective function.
 
     The diagonal shows the dependence for dimension `i` with
@@ -446,6 +446,10 @@ def plot_objective(result, levels=10, n_points=40, n_samples=250, size=2,
                             when using naive random sampling. Works with categorical values
     * `expected_minimum_samples` [float, default = None] Determines how many points should be evaluated
         to find the minimum when using 'expected_minimum' or 'expected_minimum_random'
+        
+    * `title` [str, default=None]
+        String to use as title of the figure
+
 
     Returns
     -------
@@ -504,6 +508,9 @@ def plot_objective(result, levels=10, n_points=40, n_samples=250, size=2,
 
     fig.subplots_adjust(left=0.05, right=0.95, bottom=0.05, top=0.95,
                         hspace=0.1, wspace=0.1)
+    
+    if title is not None:
+        fig.suptitle(title)
 
     val_min_1d= float("inf")
     val_max_1d= -float("inf")
@@ -593,6 +600,29 @@ def plot_objective(result, levels=10, n_points=40, n_samples=250, size=2,
                                         dim_labels=dimensions)
 
 
+def plot_objectives(results, titles=None):
+    """Pairwise dependence plots of each of the objective functions.
+    Parameters
+    ----------
+    * `results` [list of `OptimizeResult`]
+        The list of results for which to create the objective plots.
+        
+    * `titles` [list of str, default=None]
+        The list of strings of the names of the objectives used as titles in the figures
+    
+    """
+
+    if titles== None:
+        for result in results:
+            plot_objective(result, title=None )
+        return
+    else:
+        for k in range(len(results)):
+            plot_objective(results[k], title=titles[k] )   
+        return
+    
+
+
 
 def plot_evaluations(result, bins=20, dimensions=None):
     """Visualize the order in which points where sampled.
@@ -622,6 +652,9 @@ def plot_evaluations(result, bins=20, dimensions=None):
     * `ax`: [`Axes`]:
         The matplotlib axes.
     """
+    if type(result) is list:
+        result=result[0]
+    
     space = result.space
     # Convert categoricals to integers, so we can ensure consistent ordering.
     # Assign indices to categories in the order they appear in the Dimension.
