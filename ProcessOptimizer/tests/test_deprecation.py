a84a9d794bfa2f1598dd67f7dec4c432c4a4e905
AkselObdrup <75260256+AkselObdrup@users.noreply.github.com>
2021-06-21 10:22:11 +0200

    Update test_deprecation.py
    Deleted code which was not used anymore

--- ProcessOptimizer/tests/test_deprecation.py
+++ ProcessOptimizer/tests/test_deprecation.py
@@ -9,20 +9,6 @@ from ProcessOptimizer import gbrt_minimize
 from ProcessOptimizer import Optimizer
 from ProcessOptimizer.learning import ExtraTreesRegressor
 
-
-# dummy_minimize does not support same parameters so
-# treated separately
-MINIMIZERS = [gp_minimize]
-ACQUISITION = ["LCB", "PI", "EI"]
-
-
-for est, acq in product(["ET", "RF"], ACQUISITION):
-    MINIMIZERS.append(
-        partial(forest_minimize, base_estimator=est, acq_func=acq))
-for acq in ACQUISITION:
-    MINIMIZERS.append(partial(gbrt_minimize, acq_func=acq))
-
-
 def test_n_random_starts_Optimizer():
     # n_random_starts got renamed in v0.4
     et = ExtraTreesRegressor(random_state=2)
