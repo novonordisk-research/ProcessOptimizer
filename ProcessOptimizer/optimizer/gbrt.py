a26773be0d9ac6a222d6956c0c631fb936c28a22
SQBL (Søren Bertelsen) <SQBL@novonordisk.com>
2021-07-28 14:14:09 +0200

    Flake8 corrections

--- ProcessOptimizer/optimizer/gbrt.py
+++ ProcessOptimizer/optimizer/gbrt.py
@@ -33,7 +33,7 @@ def gbrt_minimize(func, dimensions, base_estimator=None,
     * `func` [callable]:
         Function to minimize. Should take a single list of parameters
         and return the objective value.
-    
+
         If you have a search-space where all dimensions have names,
         then you can use `ProcessOptimizer.utils.use_named_args` as a decorator
         on your objective function, in order to call it directly
