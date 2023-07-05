626a61ab5303882116aebece5c6e24de8ea9dffe
SRFU <srfu@novonordisk.com>
2023-07-05 14:59:01 +0200

    fix: Branin-Hoo model system works

--- ProcessOptimizer/model_systems/branin_hoo.py
+++ ProcessOptimizer/model_systems/branin_hoo.py
@@ -6,11 +6,16 @@ from ..space import Real
 
 # Define the relevant parameter space
 space = []
-space += [Real(-5,10,name='x1')]
-space += [Real(0,15,name='x2')]
+space += [Real(-5, 10, name="x1")]
+space += [Real(0, 15, name="x2")]
 
 # Set the true minimum value
 true_min = 0.397887
 
 # Create model system
-branin_hoo = ModelSystem(score, space, true_min)
+branin_hoo = ModelSystem(
+    branin,
+    space,
+    noise_model={"model_type": "constant", "noise_size": 0.02},
+    true_min=true_min,
+)
