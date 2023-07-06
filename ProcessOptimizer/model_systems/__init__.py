587c9d864f7c455798427de80ca442e36bd10009
SRFU <srfu@novonordisk.com>
2023-07-06 10:40:19 +0200

    feat: Added gold map as a model system

--- ProcessOptimizer/model_systems/__init__.py
+++ ProcessOptimizer/model_systems/__init__.py
@@ -13,6 +13,7 @@ from .benchmarks import hart3_model_system as hart3
 from .benchmarks import hart6_model_system as hart6
 from .benchmarks import poly2_model_system as poly2
 from .benchmarks import peaks_model_system as peaks
+from .gold_map import gold_map
 
 __all__ = [
     "branin_hoo",
@@ -28,4 +29,5 @@ __all__ = [
     "hart6",
     "poly2",
     "peaks",
+    "gold_map",
 ]
