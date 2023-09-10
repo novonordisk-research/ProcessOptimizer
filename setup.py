e493819e2cdaf7ea7ce6e4adcee8e8e33043d8a8
Søren <sorenbertelsen@gmail.com>
2023-09-10 22:45:22 +0200

    scipy set to 1101 to keep py38 comp

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(name='ProcessOptimizer',
                          'numpy==1.24.4',
                          'pyYAML==6.0.1',
                          'scikit-learn==1.3.0',
-                         'scipy==1.11.2',
+                         'scipy==1.10.1',
                          'six==1.16.0',
                          'tornado==6.3.3',
                          ]
