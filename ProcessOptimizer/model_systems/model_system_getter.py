a087ed9ea7a6d678b789fc6bdd3b4eeb8067ce42
Søren Furbo <srfu@novonordisk.com>
2024-07-09 12:18:26 +0200

    fix_ Synonym for branin_no_noise

--- ProcessOptimizer/model_systems/model_system_getter.py
+++ ProcessOptimizer/model_systems/model_system_getter.py
@@ -18,7 +18,7 @@ def get_model_system(model_system: str) -> ModelSystem:
     """
     if model_system == "branin_hoo":
         return create_branin(noise=True)
-    elif model_system == "branin_no_noise":
+    elif model_system in ["branin_no_noise", "branin_hoo_no_noise"]:
         return create_branin(noise=False)
     else:
         raise ValueError(f"Model system {model_system} not found.")
