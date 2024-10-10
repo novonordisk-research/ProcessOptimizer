364e011e785977ceebd32acfeebae9e846f0ce1b
Søren Furbo <srfu@novonordisk.com>
2024-10-10 15:09:56 +0200

    doc: Seeding LHS in benchmarking

--- ProcessOptimizer/model_systems/benchmark.py
+++ ProcessOptimizer/model_systems/benchmark.py
@@ -19,16 +19,17 @@ def run_test_optimization(
     Returns
     -------
     tuple
-        A tuple containing the input test (for reference), the number of evaluations done
-        and whether the optimization was successful.
+        A tuple containing the input test (for reference), the number of evaluations
+        done and whether the optimization was successful.
     """
-    model_system = get_model_system(test["model_system_name"], seed = test["seed"])
+    model_system = get_model_system(test["model_system_name"], seed=test["seed"])
     model_system.noise_size = model_system.noise_size*test["noise_level"]
     objective_range = model_system.true_max - model_system.true_min
     target = model_system.true_min + test["target_level"] * objective_range
     optimizer = Optimizer(
         dimensions=model_system.space,
-        n_initial_points=test["n_initial_points"]
+        n_initial_points=test["n_initial_points"],
+        random_state=test["seed"],
     )
     for _ in range(test["experiment_budget"]):
         finished = False
