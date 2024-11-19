3df9004951a36de5718cda1884950c0eff4fe9a5
RUCT <ruct@novonordisk.com>
2024-11-19 09:44:03 +0100

    light cleaning before merging back

--- ProcessOptimizer/doe/classical_designs.py
+++ ProcessOptimizer/doe/classical_designs.py
@@ -1,13 +1,9 @@
-import math
-import warnings
+# import math
+# import warnings
 
-import numpy as np
-import patsy
+# import numpy as np
 
-from .doe_transform import doe_to_real_space
-
-# Defining a number of helper functions for the optimal design of experiments
-
+# from .doe_transform import doe_to_real_space
 
 # In here goes some classical designs.
 # They could be adapted from https://github.com/tisimst/pyDOE
