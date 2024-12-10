42a397cf47762a310b3d204e1e72c8c204b5e803
RUCT <ruct@novonordisk.com>
2024-12-10 10:21:12 +0100

    Slight linting

--- ProcessOptimizer/doe/optimal_design.py
+++ ProcessOptimizer/doe/optimal_design.py
@@ -3,13 +3,10 @@ import math
 import numpy as np
 import patsy
 
-from .doe_transform import doe_to_real_space
-from .doe_utils import (
-    generate_replicas_and_sort,
-    round_design_point_values,
-    sanitize_names_for_patsy,
-)
 from ..space import Categorical
+from .doe_transform import doe_to_real_space
+from .doe_utils import (generate_replicas_and_sort, round_design_point_values,
+                        sanitize_names_for_patsy)
 
 # Defining a number of helper functions for the optimal design of experiments
 
