76ea71d793fa9d357b8fb43f6ae385809e7bf131
RUCT <ruct@novonordisk.com>
2024-12-16 12:39:40 +0100

    doe is module in setup

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
         "ProcessOptimizer.model_systems",
         "ProcessOptimizer.model_systems.data",
         "ProcessOptimizer.optimizer",
+        "ProcessOptimizer.samples.doe"
         "ProcessOptimizer.space",
         "ProcessOptimizer.utils",
         "ProcessOptimizer.learning.gaussian_process",
