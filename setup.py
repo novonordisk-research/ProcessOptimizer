96f052945d4a2fdf2d01c803d68279ea6ddbe791
RUCT <ruct@novonordisk.com>
2024-12-17 14:17:39 +0100

    forgotten comma in setup fixed

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         "ProcessOptimizer.model_systems",
         "ProcessOptimizer.model_systems.data",
         "ProcessOptimizer.optimizer",
-        "ProcessOptimizer.samples.doe"
+        "ProcessOptimizer.samples.doe",
         "ProcessOptimizer.space",
         "ProcessOptimizer.utils",
         "ProcessOptimizer.learning.gaussian_process",
