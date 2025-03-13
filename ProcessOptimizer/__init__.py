d61283466268cff80ba1a012da28ff41f39d37d1
Søren Furbo <104423389+SRFU-NN@users.noreply.github.com>
2025-03-13 11:24:05 +0100

    test: Test new csv file with data

--- ProcessOptimizer/__init__.py
+++ ProcessOptimizer/__init__.py
@@ -38,6 +38,8 @@ from .plots import plot_objective, plot_objectives
 from .plots import plot_evaluations, plot_convergence
 from .plots import plot_Pareto, plot_expected_minimum_convergence
 
+from .model_systems.test_data import test_data
+
 __version__ = "0.9.3"
 
 
