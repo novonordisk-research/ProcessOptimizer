beb60c9d5e3a083e142b4299cae2944795513ec4
RuneChristensen-NN <>
2025-04-01 09:46:32 +0200

    Implemented warning message if multiobjt and number of Int dims larger than 4

--- ProcessOptimizer/optimizer/optimizer.py
+++ ProcessOptimizer/optimizer/optimizer.py
@@ -19,7 +19,7 @@ from sklearn.utils import check_random_state
 from ..acquisition import _gaussian_acquisition
 from ..acquisition import gaussian_acquisition_1D
 from ..learning import cook_estimator, GaussianProcessRegressor, has_gradients
-from ..space import Categorical
+from ..space import Categorical, Integer
 from ..space import Space, normalize_dimensions
 from ..space.constraints import Constraints, SumEquals
 from ..utils import check_x_in_space
@@ -185,6 +185,18 @@ class Optimizer(object):
 
         # Set the number of objectives
         self.n_objectives = n_objectives
+
+        # Warning to close issue # 326
+        if n_objectives > 1:
+            n_dim_int = len(
+                [dim for dim in dimensions if isinstance(dim, Integer)]
+            )
+            if n_dim_int > 4:
+                warnings.warn(
+                    f"The number of integer dimensions is {n_dim_int}. This"
+                    " may impact performance. Performance could be improved by"
+                    " changing to Float dimensions where possible"
+                )
 
         # Configure acquisition function
 
