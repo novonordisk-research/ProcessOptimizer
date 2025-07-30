f5ef187d898589cb7cf6509e0e49e2aedcb136da
Søren Furbo <srfu@novonordisk.com>
2025-07-30 08:56:17 +0200

    fix: Copying suggestor definition
    This preserves keywords that are popped in the original definition

--- ProcessOptimizer/XpyriMentor/suggestors/suggestor_factory.py
+++ ProcessOptimizer/XpyriMentor/suggestors/suggestor_factory.py
@@ -72,14 +72,15 @@ def suggestor_factory(
     if rng is None:
         rng = np.random.default_rng(1)
     if not definition:  # If definition is None or empty, return DefaultSuggestor.
-        suggestor_type = "Default"
-    else:
-        try:
-            suggestor_type = definition.pop("suggestor_name")
-        except KeyError as e:
-            raise ValueError(
-                f"Missing 'suggestor_name' key in suggestor definition: {definition}"
-            ) from e
+        definition = {"suggestor_name": "Default"}
+    definition = definition.copy()
+    # Copying to preserve original before we start popping keys
+    try:
+        suggestor_type = definition.pop("suggestor_name")
+    except KeyError as e:
+        raise ValueError(
+            f"Missing 'suggestor_name' key in suggestor definition: {definition}"
+        ) from e
     if suggestor_type not in suggestors:
         raise ValueError(f"Unknown suggestor name: {suggestor_type}")
     return suggestors[suggestor_type].create_from_definition(
