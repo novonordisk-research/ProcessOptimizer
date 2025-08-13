9997548694eb32273a9c8672f21d22a62c58cfd4
Søren Furbo <srfu@novonordisk.com>
2025-08-13 09:22:53 +0200

    feat: ConstantSuggestor requires an explicit point in real space.

--- ProcessOptimizer/XpyriMentor/suggestors/constant_suggestor.py
+++ ProcessOptimizer/XpyriMentor/suggestors/constant_suggestor.py
@@ -1,9 +1,8 @@
 from __future__ import annotations
-from typing import Any, Union, Iterable
+from typing import Any, Iterable
 
 import numpy as np
 from ProcessOptimizer.space import Space
-from ProcessOptimizer.utils import is_listlike
 
 
 class ConstantSuggestor:
@@ -11,28 +10,17 @@ class ConstantSuggestor:
     A suggestor that always returns the same point.
     """
 
-    def __init__(
-        self, space: Space, point: Union[list, float] = 0.5, convert: bool = True
-    ):
+    def __init__(self, space: Space, point: list[Any]):
         """
         Initialize the suggestor.
 
         Parameters:
         * `space` [Space]:
             The search space.
-        * `point` [list or float]:
-            The point to suggest. If not a list, it is converted to a list with
-            `space.n_dims` identical elements. Default is 0.5, which, if `convert` is
-            `True`, makes the suggestor return the center point of `space`.
-        * `convert` [bool]:
-            If `True` (default), the point is converted to a point in the space with
-            `space.sample`
+        * `point` [list]:
+            The point to suggest. It has to be a point in the search space.
         """
         self.space = space
-        if not is_listlike(point):
-            point = [point] * space.n_dims
-        if convert:
-            point = space.sample(point)
         self.point = np.array(point)
 
     def suggest(
@@ -66,4 +54,4 @@ class ConstantSuggestor:
         n_objectives,
         rng: np.random.Generator,
     ) -> ConstantSuggestor:
-        return ConstantSuggestor(space=space, **definition)
+        return ConstantSuggestor(space=space, point=definition["point"])
--- ProcessOptimizer/tests/test_suggestors/test_constant_suggestor.py
+++ ProcessOptimizer/tests/test_suggestors/test_constant_suggestor.py
@@ -9,26 +9,20 @@ from ProcessOptimizer.XpyriMentor.suggestors import (
 
 def test_initialization():
     space = space_factory([[0, 1], [0, 1]])
-    suggestor = ConstantSuggestor(space=space)
+    suggestor = ConstantSuggestor(space=space, point=[0.5, 0.5])
     assert isinstance(suggestor, ConstantSuggestor)
 
 
 def test_protocol():
     space = space_factory([[0, 1], [0, 1]])
-    suggestor = ConstantSuggestor(space=space)
+    suggestor = ConstantSuggestor(space=space, point=[0.5, 0.5])
     assert isinstance(suggestor, Suggestor)
     assert isinstance(suggestor, CreatableSuggestor)
 
 
-def test_default_value():
-    space = space_factory([[0.0, 1.0]])
-    suggestor = ConstantSuggestor(space=space)
-    assert all(suggestor.suggest(Xi=[], Yi=[], n_asked=1) == [[0.5]])
-
-
 def test_given_value():
     space = space_factory([[0.0, 1.0]])
-    suggestor = ConstantSuggestor(space=space, point=0.2)
+    suggestor = ConstantSuggestor(space=space, point=[0.2])
     assert suggestor.suggest(Xi=[], Yi=[], n_asked=1) == [[0.2]]
 
 
@@ -38,14 +32,6 @@ def test_different_given_values():
     assert all(suggestor.suggest(Xi=[], Yi=[], n_asked=1)[0] == [0.2, 0.8])
 
 
-def test_convert():
-    space = space_factory([[-10.0, 10.0]])
-    converting_suggestor = ConstantSuggestor(space=space, point=0.5)
-    assert converting_suggestor.suggest(Xi=[], Yi=[], n_asked=1) == [[0.0]]
-    non_converting_suggestor = ConstantSuggestor(space=space, point=0.5, convert=False)
-    assert non_converting_suggestor.suggest(Xi=[], Yi=[], n_asked=1) == [[0.5]]
-
-
 def test_factory():
     space = space_factory([[-10.0, 10.0], [0, 10]])
     suggestor = suggestor_factory(
@@ -53,7 +39,6 @@ def test_factory():
         definition={
             "suggestor_name": "Constant",
             "point": [0.2, 0.8],
-            "convert": False,
         },
     )
     assert isinstance(suggestor, ConstantSuggestor)
